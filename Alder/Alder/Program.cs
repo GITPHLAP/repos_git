cbd6c0d4085d2e143ed88e3deb75e61a084335ee
GITPHLAP <kjedtoft@gmail.com>
2020-01-29 13:20:39 +0100

    Program

---
+++ Alder/Alder/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Alder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Indsætter værdierne
+            Console.WriteLine("Skriv dit navn");
+            string Name = Console.ReadLine();
+            Console.WriteLine("Skriv din alder");
+            uint Age = uint.Parse(Console.ReadLine());
+
+            //Finder ud af hvor alderen passer til
+            if (Age < 3)//hvis alderen er mindre end 3
+            {
+                Console.WriteLine($"{Name} du må gå med ble");
+            }
+            else if (Age >= 18)//hvis alderen er 18 og derover
+            {
+                Console.WriteLine($"{Name} du må stemme og køre bil");
+            }
+            else if (Age >= 15)//hvis alderen er 15 og derover, hvis den er over 18 bliver den fanget af betingelsen ovenover
+            {
+                Console.WriteLine($"{Name} du må drikke");
+            }
+            else if (Age >= 3)//hvis alderen er 3 og derover
+            {
+                Console.WriteLine($"{Name} du må ingenting");
+            }
+        }
+    }
+}
