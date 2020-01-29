57835b396449444ad6fdb065815d543d5d7a106f
GITPHLAP <kjedtoft@gmail.com>
2020-01-29 16:18:27 +0100

    Upload one more program

---
+++ Arrays opgave 5/Arrays opgave 5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Arrays_opgave_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float[,,,,] tabel = new float[5, 5, 5, 5, 5]; //Jeg laver et todimensionelt array på på 5 rækker og 5 kolonner
+            int counter = 1;
+            //dette for-loop bliver brugt til at indsætte værdierne i arrayet.
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    tabel[i, j, i, j, i] = counter;
+                    counter++;                    
+                }
+            }
+            //Dette for loop bliver brugt til at udskrive i det som en tabel
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write(tabel[i, j, i, j, i] + "      ");
+                }
+                Console.Write(Environment.NewLine);
+            }
+        }
+    }
+}
