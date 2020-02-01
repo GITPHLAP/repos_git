a948ddda3505ff5546a33003fc602581c3d40968
GITPHLAP <kjedtoft@gmail.com>
2020-02-01 15:53:40 +0100

    Metode opgaver

---
+++ Metoder_Filer_Framework/Metoder_Filer_Framework/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Metoder_Filer_Framework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                
+                string input1 = "";//Laver min variable global så jeg kan bruge den flere steder 
+                Console.WriteLine("Tilføj fil(1)    Slet fil(2)     Vis filer(3)    Tilføj mappe(4)     Søg efter filer(5)      Søg efter type(6)       Luk programmet(7)");
+                int whichone = int.Parse(Console.ReadLine());//Bruger input
+                switch (whichone) //Valgmulighederne
+                {
+                    case 1:
+                        Console.WriteLine("Skriv navnet på filen.");
+                        input1 = Console.ReadLine();//Brugerinput
+                        AddFile(input1);//Bruger min metode
+                        break;
+                    case 2:
+                        Console.WriteLine("Skriv navnet på filen i den lokale folder du vil slette");
+                        input1 = Console.ReadLine();
+                        DeleteFile(input1);
+                        break;
+                    case 3:
+                        Console.WriteLine(@"Viser filer og mapper fra C:\");
+                        ViewFileAndFolder();
+                        break;
+                    case 4:
+                        Console.WriteLine("Tiltøj en mappe i den lokale mappe som programmet ligger i");
+                        input1 = Console.ReadLine();
+                        AddFolder(input1);
+                        break;
+                    case 5:
+                        Console.WriteLine("Søg efter et navn på en fil");
+                        input1 = Console.ReadLine();
+                        SearchFileName(input1);
+                        break;
+                    case 6:
+                        Console.WriteLine("Søg efter fil type");
+                        input1 = Console.ReadLine();
+                        SearchFileType(input1);
+                        break;
+                    case 7:
+                        Environment.Exit(0);//Lukker programmet
+                        break;
+                    default:
+                        break;
+                }
+                Console.ReadLine();
+            }
+        }
+        //Alle mine metoder
+        private static void SearchFileType(string input)
+        {
+            foreach (var files in Directory.GetFiles(@".\Droids", $"*.{input}"))//For hver fil der er i det valgte directory bliver der udskrevet sti'en
+            {
+                Console.WriteLine(files);
+            }
+        }
+
+        private static void SearchFileName(string foldernameinput)
+        {
+            foreach (var files in Directory.GetFiles(@"C:\Users\kjedt\source\repos_git", $"{foldernameinput}", SearchOption.AllDirectories)) //For hver fil der er i det valgte directory bliver der udskrevet sti'en
+            {
+                Console.WriteLine(files);
+            }
+        }
+
+        private static void AddFolder(string foldername)
+        {
+            Directory.CreateDirectory($@".\{foldername}");//tilføjer en mappe i mappen programmet ligger i med det navn som brugeren skriver 
+        }
+
+        private static void ViewFileAndFolder()
+        {
+            foreach (var files in Directory.GetFiles(@"C:\Users\kjedt\source\repos_git", "*",SearchOption.AllDirectories)) //viser alle filer i mappen repos_git.
+            {
+                Console.WriteLine(files);
+            }
+        }
+
+        private static void DeleteFile(string filename)// sletter en fil med passende navn som brugeren skriver ind.
+        {
+            File.Delete($@".\{filename}.txt");
+        }
+
+        private static void AddFile(string filename) // tilføjer en fil med passende navn som brugeren skriver ind.
+        {
+            FileStream file = new FileStream($@".\{filename}.txt", FileMode.Create);
+            StreamWriter writer = new StreamWriter(file);
+            writer.Close();
+        }
+    }
+}
---
+++ Metoder_lommeregner/Metoder_lommeregner/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metoder_lommeregner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Efter aftale med mikkel har vi lavet den her opgave istedet for
+            //Declare variabler jeg skal bruge senere
+            double resultaftermethod = 0;
+            double number1 = 0;
+            double number2 = 0;
+            //Giver brugeren et valg for hvad der skal ske
+            Console.WriteLine("Vælg mellem nedenstående:");
+            Console.WriteLine("Tast 1: for at plus tal 1 med tal 2");
+            Console.WriteLine("Tast 2: for at minus tal 1 med tal 2");
+            Console.WriteLine("Tast 3: for at gange tal 1 med tal 2");
+            Console.WriteLine("Tast 4: for at dividere tal 1 med tal 2");
+            Console.WriteLine("Tast 5: for at kvadratrod tal 1 ");
+            Console.WriteLine("Tast 6: for at opløft tal 1 med tal 2");
+            Console.WriteLine("Tast 7: for Rest");
+            int selected = int.Parse(Console.ReadLine());//bruger input
+            //Valgmulighederne
+            switch (selected)
+            {
+                case 1:
+                    Console.WriteLine("Tal1 + Tal2");
+                    Console.WriteLine("Skriv tal 1");
+                    number1 = int.Parse(Console.ReadLine());//bruger input
+                    Console.WriteLine("Skriv tal 2");
+                    number2 = int.Parse(Console.ReadLine());
+                    resultaftermethod = plus(number1, number2);//Resultatet hvor jeg kalder min metode
+                    Console.WriteLine("Resultatet af {0} {1} {2} = {3}", number1, "+", number2, resultaftermethod);//udskriver resultatet med de værdiér som brugeren har skrevet
+                    break;
+                case 2:
+                    Console.WriteLine("Tal1 + Tal2");
+                    Console.WriteLine("Skriv tal 1");
+                    number1 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Skriv tal 2");
+                    number2 = int.Parse(Console.ReadLine());
+                    resultaftermethod = minus(number1, number2);
+                    Console.WriteLine("Resultatet af {0} {1} {2} = {3}", number1, "-", number2, resultaftermethod);
+                    break;
+                case 3:
+                    Console.WriteLine("Tal1 + Tal2");
+                    Console.WriteLine("Skriv tal 1");
+                    number1 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Skriv tal 2");
+                    number2 = int.Parse(Console.ReadLine());
+                    resultaftermethod = gange(number1, number2);
+                    Console.WriteLine("Resultatet af {0} {1} {2} = {3}", number1, "*", number2, resultaftermethod);
+                    break;
+                case 4:
+                    Console.WriteLine("Tal1 + Tal2");
+                    Console.WriteLine("Skriv tal 1");
+                    number1 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Skriv tal 2");
+                    number2 = int.Parse(Console.ReadLine());
+                    resultaftermethod = dividere(number1, number2);
+                    Console.WriteLine("Resultatet af {0} {1} {2} = {3}", number1, "/", number2, resultaftermethod);
+                    break;
+                case 5:
+                    Console.WriteLine("Tal1 + Tal2");
+                    Console.WriteLine("Skriv tal 1");
+                    number1 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Skriv tal 2");
+                    number2 = int.Parse(Console.ReadLine());
+                    resultaftermethod = kvadratrod(number1);
+                    Console.WriteLine("Resultatet af kvadratrod af{0} = {1}", number1, resultaftermethod);
+                    break;
+                case 6:
+                    Console.WriteLine("Tal1 + Tal2");
+                    Console.WriteLine("Skriv tal 1");
+                    number1 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Skriv tal 2");
+                    number2 = int.Parse(Console.ReadLine());
+                    resultaftermethod = potens(number1, number2);
+                    Console.WriteLine("Resultatet af {0} opløftet i {1} = {2}", number1, number2, resultaftermethod);
+                    break;
+                case 7:
+                    Console.WriteLine("Find restenværdien ved tal1 af tal2 ");
+                    Console.WriteLine("Skriv tal 1");
+                    number1 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Skriv tal 2");
+                    number2 = int.Parse(Console.ReadLine());
+                    resultaftermethod = moduls(number1, number2);
+                    Console.WriteLine("Restværdien af {0} / {1} = {2}", number1, number2, resultaftermethod);
+                    break;
+                default:
+                    break;
+            }
+        }
+        //Mine metoder 
+        public static double plus(double num1, double num2)
+        {
+            double result = num1 + num2;
+            return result;
+        }
+        public static double minus(double num1, double num2)
+        {
+            double result = num1 - num2;
+            return result;
+        }
+        public static double gange(double num1, double num2)
+        {
+            double result = num1 * num2;
+            return result;
+        }
+        public static double dividere(double num1, double num2)
+        {
+            double result = num1 / num2;
+            return result;
+        }
+        public static double potens(double num1, double num2)
+        {
+            double result = Math.Pow(num1, num2);
+            return result;
+        }
+        public static double kvadratrod(double num1)
+        {
+            double result = Math.Sqrt(num1);
+            return result;
+        }
+        public static double moduls(double num1, double num2)
+        {
+            double result = num1 % num2;
+            return result;
+        }
+
+    }
+}
