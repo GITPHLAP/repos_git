4e2b838272b5d4848e29ee1bdbeaffc2dbbeaaba
GITPHLAP <kjedtoft@gmail.com>
2020-02-24 12:53:29 +0100

    Vaskmaskine opgave

---
+++ Vaskemaskine/Vaskemaskine/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vaskemaskine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WashingMachine wash = new WashingMachine();
+
+            while (true)
+            {
+                Console.WriteLine("Vælg P for Power");
+                Console.WriteLine("Vælg F for Fill");
+                Console.WriteLine("Vælg W for Wash");
+                Console.WriteLine("Vælg EW for Eco Wash");
+                Console.WriteLine("Vælg S for Spin");
+
+                string input = Console.ReadLine();
+                switch (input) //laver en switch case udfra brugerens input
+                {
+                    case "P":
+
+                        wash.turnonandoff();
+                        break;
+                    case "F":
+                        wash.Filling();
+                        break;
+                    case "W":
+                        wash.Washing();
+                        break;
+                    case "EW":
+                        wash.EcoWashing();
+                        break;
+                    case "S":
+                        wash.Spinning();
+                        break;
+                    default:
+                        break;
+                }
+
+
+                Console.ReadLine();
+            }
+            
+            
+        }
+    }
+}
---
+++ Vaskemaskine/Vaskemaskine/WashingMachine.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vaskemaskine
+{
+    class WashingMachine
+    {
+        private bool power; //laver en tænd og sluk kontakt 
+
+        public bool Power //gøre den public
+        {
+            set
+            {
+                power = value;
+            }
+            get
+            {
+                return power;
+            }
+
+        }
+
+
+        public void turnonandoff() //metode til at slukke og tænde maskinen 
+        {
+            if (this.power == true)
+            {
+                this.power = false;
+                Console.WriteLine("Maskinen er slukket");
+            }
+            else
+            {
+                this.power = true;
+                Console.WriteLine("Maskinen er tændt");
+            }
+            
+        }
+
+
+        public void Filling()
+        {
+            if (this.power == false)//tester om maskinen er slukket 
+            {
+                return;
+            }
+            Console.WriteLine("Vaskmaskinen er i gang med at blive fyldt");
+            for (int i = 10; i >= 0; i--)
+            {
+                Console.WriteLine($"Resterende tid {i}");// laver en tæller 
+            }
+        }
+        public void Washing()
+        {
+            if (this.power == false)
+            {
+                return;
+            }
+            Console.WriteLine("Vaskmaskinen er i gang med at vaske");
+            for (int i = 20; i >= 0; i--)
+            {
+                Console.WriteLine($"Resterende tid {i}");
+            }
+        }
+        public void EcoWashing()
+        {
+            if (this.power == false)
+            {
+                return;
+            }
+            Console.WriteLine("Vaskmaskinen er i gang med at vaske ECO");
+            for (int i = 20; i >= 0; i--)
+            {
+                Console.WriteLine($"Resterende tid {i}");
+            }
+        }
+        public void Spinning()
+        {
+            if (this.power == false)
+            {
+                return;
+            }
+            Console.WriteLine("Vaskmaskinen er i gang med at spin my head round and round");
+            for (int i = 25; i >= 0; i--)
+            {
+                Console.WriteLine($"Resterende tid {i}");
+            }
+        }
+    }
+}
