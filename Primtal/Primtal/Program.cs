ef83c6a23b5cc17d9cae21ca55baf94f0a335e7b
GITPHLAP <kjedtoft@gmail.com>
2020-02-25 10:36:29 +0100

    Primtal

---
+++ Primtal/Primtal/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primtal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> primes = new Queue<int>();
+            primes.Enqueue(2);
+            primes.Enqueue(3);
+            primes.Enqueue(5);
+            primes.Enqueue(7);
+            primes.Enqueue(11);
+
+            int total = 0;
+            foreach (var item in primes)
+            {
+                total += item;
+            }
+            Console.WriteLine(total);
+
+            Console.ReadLine();
+        }
+    }
+}
