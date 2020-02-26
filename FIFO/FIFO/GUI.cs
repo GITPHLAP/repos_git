2cf0b5de964683b8755e729c792edad41e133e1c
GITPHLAP <kjedtoft@gmail.com>
2020-02-26 08:14:35 +0100

    Fifo

---
+++ FIFO/FIFO/GUI.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIFO
+{
+    class GUI
+    {
+        public void menu()
+        {
+            Persons logic = new Persons();//opretter en person jeg kan bruge til at kalde min metoder
+
+
+            Console.WriteLine("1. add");
+            Console.WriteLine("2. fjern den første person der kom til festen");
+            Console.WriteLine("3. se antal");
+            Console.WriteLine("4. se ældste og yngste");
+            Console.WriteLine("5. find en person");
+            Console.WriteLine("6. se alle");
+            Console.WriteLine("7. luk programmet");
+
+
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    Console.WriteLine("skriv navn");
+                    string nameinput = Console.ReadLine();
+                    Console.WriteLine("Skriv alder");
+                    int ageinput = Int32.Parse(Console.ReadLine());//konvetere mit input til INT
+                    Console.WriteLine("Skriv køn");
+                    string genderinput = Console.ReadLine();
+                    Persons person = new Persons(nameinput, ageinput, genderinput);//kalder en konstruktør som tilføjer personen når jeg kalder den
+                    break;
+                case "2":
+                    logic.removeperson();//kalder mine metoder fra person
+                    break;
+                case "3":
+                    logic.number();
+                    break;
+                case "4":
+                    logic.MinAndMax();
+                    break;
+                case "5":
+                    Console.WriteLine("Skriv navnet på den person du vil finde");
+                    Console.WriteLine("OBS: Du skal søge på hele navnet");
+                    string namefindinput = Console.ReadLine();
+                    logic.Findsomeone(namefindinput);
+                    break;
+                case "6":
+                    logic.Showall();
+                    break;
+                case "7":
+                    Environment.Exit(1);//lukker programmet
+                    break;
+                default:
+                    break;
+            }
+
+        }
+    }
+}
---
+++ FIFO/FIFO/Persons.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIFO
+{
+    class Persons
+    {
+        //opretter felterne prvate
+        private string name;
+        private int age;
+        private string gender;
+
+
+        static Queue<Persons> guests = new Queue<Persons>(); //Opretter min kø, jeg gør den statisk fordi at det skal være den samme kø hele tiden
+
+
+
+
+        #region get/set
+        //opretter felterne public
+        public string Name
+        {
+            set
+            {
+                this.name = value;
+            }
+            get
+            {
+                return name;
+            }
+        }
+        public int Age
+        {
+            set
+            {
+                this.age = value;
+            }
+            get
+            {
+                return age;
+            }
+        }
+        public string Gender
+        {
+            set
+            {
+                this.gender = value;
+            }
+            get
+            {
+                return gender;
+            }
+        }
+        #endregion get/set
+
+        // laver en default konstruktør 
+        public Persons()
+        {
+            
+        }
+
+        //laver en konstruktør som tilføjer inputtet til listen
+        public Persons(string Iname, int Iage, string Igender)
+        {
+            this.name = Iname;
+            this.age = Iage;
+            this.gender = Igender;
+            guests.Enqueue(this);
+        }
+
+        //Metode til at vise alle
+        public void Showall()
+        {
+            foreach (var item in guests)//køre hvergang der er noget i køen
+            {
+                Console.WriteLine(item.name);//udskriver navnet
+            }
+        }
+
+        public void removeperson()
+        {
+            guests.Dequeue();//fjerne den foreste person i køen
+        }
+        public void number()
+        {
+            Console.WriteLine("Der er " + guests.Count() + " gæster");//finder antalet af gæster
+        }
+
+        public void MinAndMax()//finder minimum og maximum
+        {
+            Console.WriteLine("Den ældste gæst til festen er "+guests.Max(guest => guest.age)+" år gammel");
+            Console.WriteLine("Den yngste gæst til festen er "+guests.Min(guest => guest.age)+" år gammel");
+        }
+
+        public void Findsomeone(string inputname)//leder efter det navn som er blevet skrevet
+        {
+            foreach (var item in guests)
+            {
+                if (inputname == item.name)
+                {
+                    Console.WriteLine("Navn: "+item.name);
+                    Console.WriteLine("Alder: "+item.age);
+                    Console.WriteLine("Køn: "+item.gender);
+                }
+            }
+        }
+    }
+}
